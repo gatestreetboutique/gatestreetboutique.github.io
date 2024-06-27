aa80f90379f1270b1b0dca9a419ff830973b7bfa
James Cammarata <jimi@sngx.net>
2024-06-27 16:14:09 -0500

    Updating test

--- pelicanconf.py
+++ pelicanconf.py
@@ -15,23 +15,9 @@ TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
-# Blogroll
-LINKS = (
-    ("Pelican", "https://getpelican.com/"),
-    ("Python.org", "https://www.python.org/"),
-    ("Jinja2", "https://palletsprojects.com/p/jinja/"),
-    ("You can modify those links in your config file", "#"),
-)
-
-# Social widget
-SOCIAL = (
-    ("You can add links in your config file", "#"),
-    ("Another social link", "#"),
-)
-
 DEFAULT_PAGINATION = False
 
 # Uncomment following line if you want document-relative URLs when developing
 # RELATIVE_URLS = True
 
-THEME = 'themes/pelican-bootstrap3'
+THEME = 'themes/Casper2Pelican'
